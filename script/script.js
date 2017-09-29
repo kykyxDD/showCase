52410f9c323d3b2a4da31a7ebf54f8281790d36a
NastyaShcherbakova <kykyxDD@gmail.com>
2017-09-29 19:49:51 +0300

    update

---
+++ script/script.js
@@ -0,0 +1,344 @@
+function briefBanner(){
+	var parent
+	var self = this
+	var view, cont_view
+	this.index_temt = 0;
+	this.list_data = [];
+	this.init = function(){
+		parent = createElem('div', 'banner', document.body)
+		console.log(parent)
+
+		this.banner = parent
+
+
+		var cont_start = createElem('div', 'cont_start', parent);
+		var cont_btn = createElem('div', 'cont_btn', cont_start);
+		var btn_start = createElem('div', 'btn_start', cont_btn);
+		btn_start.innerHTML = 'start';
+
+		btn_start.addEventListener('click', function(){
+
+			var data = self.list_data[self.index_temt]//self.json_xml.data[self.index_temt].data;
+			self.fullScreen()
+			self.startBanner(data)
+		});
+
+		view = createElem('div', 'view', parent)
+		cont_view = createElem('div', 'cont_view', view)
+
+
+		var url = './data/data.xml';
+		this.loadXML(url);
+	}
+
+	this.loadXML = function(filename){
+
+		// try {
+		//     var xmlhttp = new XMLHttpRequest();
+		//     xmlhttp.open("GET", xmlfile, false);
+		// } catch (Exception) {
+		//     var ie = (typeof window.ActiveXObject != 'undefined');
+
+		//     if (ie){
+		//         xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+		//         xmlDoc.async = false;
+		//         while(xmlDoc.readyState != 4) {};
+		//         xmlDoc.load(xmlfile);
+		//         readXML();
+		//         xmlloaded = true;
+		//     } else {
+		//         xmlDoc = document.implementation.createDocument("", "", null);
+		//         xmlDoc.onload = readXML;
+		//         xmlDoc.load(xmlfile);
+		//         xmlloaded = true;
+		//     }
+		// }
+
+		// if (!xmlloaded){
+		//     xmlhttp.setRequestHeader('Content-Type', 'text/xml')
+		//     xmlhttp.send("");
+		//     xmlDoc = xmlhttp.responseXML;
+		//     readXML();
+		//     xmlloaded = true;
+		// }
+		var self = this;
+
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				self.createBanner()
+				self.runData(this.responseXML)
+			}
+		};
+		xhttp.open("GET", filename, true);
+		xhttp.send();
+	}
+
+	this.createBanner = function(){
+
+		var image = createElem('div', 'img_product', cont_view)
+
+		
+		var line_top = createElem('div', 'line_top', cont_view);
+
+		var cont_bg_top = createElem('div', 'bg_top', line_top);
+		var text_top = createElem('div', 'text_top', line_top)
+
+
+		var blue_line = createElem('div', 'blue_line', cont_view);
+		var itm_center = createElem('div', 'circle', cont_view);
+
+		var center_text1 = createElem('div', 'text1', itm_center)
+		center_text1.innerHTML = 'save';
+		var center_price = createElem('div', 'price', itm_center)
+		var line_bottom = createElem('div', 'line_bottom', cont_view);
+		var cont_text = createElem('div', 'cont_text', line_bottom);
+		var text_1 = createElem('div', 'text text_1', cont_text)
+		var text_2 = createElem('div', 'text text_2', cont_text)
+
+
+		var desc = createElem('div', 'desc', cont_view)
+
+
+		var cont_was = createElem('div', 'was', blue_line);
+		var name_was = createElem('div', 'name', cont_was);
+		name_was.innerHTML = 'WAS';
+		var list_was = createElem('div', 'list_was', cont_was);
+
+		var text_now = createElem('div', 'text_now', blue_line);
+		text_now.innerHTML = 'now only';
+		var text_price = createElem('div', 'text_price', blue_line);
+
+		var brand = createElem('div', 'info_brand', blue_line)
+
+		var btn_switch = createElem('div', 'btn_switch', cont_view);
+		btn_switch.innerHTML = 'Next';
+
+		btn_switch.addEventListener('click', function(){
+			self.switchData()
+		})
+
+
+		this.elem_banner = {
+			center: itm_center,
+			desc: desc,
+			center_text1: center_text1,
+			center_price: center_price,
+			red_line_bottom: [
+				text_1,text_2
+			],
+			text_now: text_now,
+			red_line_top: text_top,
+			blue_line_top: blue_line,
+			text_price: text_price,
+			brand: brand,
+			img_product: image,
+			list_was: list_was
+		}
+	};
+	this.switchData = function(){
+		var next_index = (this.index_temt+1)%this.json_xml.data.length;
+
+		var data = this.list_data[next_index];
+		this.index_temt = next_index
+
+		// console.log('data',data)
+
+		this.startBanner(data)
+	}
+
+	this.runData = function(data){
+		var xml = data.children[0]
+		// console.log(xml)
+
+		this.json_xml = {};
+
+		for(var i = 0; i < xml.children.length; i++){
+			var child = xml.children[i]
+			// console.log('child', child)
+			var key = child.tagName.toLocaleLowerCase()
+			var elem
+			if(!child.children.length) {
+				elem = child.innerHTML
+			} else {
+				elem = child.children
+			}
+			this.json_xml[key] = elem 
+		}
+
+		if(this.json_xml.data && this.json_xml.data.length) {
+			var itm_data = this.json_xml.data
+			for(var i = 0; i < itm_data.length; i++){
+				var itm = itm_data[i]
+				// console.log(itm)
+				var parse_data = {};
+				if(!itm.children.length) continue
+				for(var j = 0; j < itm.children.length; j++){
+					var key = itm.children[j].tagName.toLocaleLowerCase();
+					if(key.indexOf('feature')>=0){
+						key = 'feature';
+
+						var index = +itm.children[j].tagName.toLocaleLowerCase().replace(key, '')
+
+						if(!parse_data[key]) parse_data[key] = [];
+						parse_data[key][index-1] = itm.children[j].innerHTML
+					} else if(key.indexOf('wasprice') >= 0){
+						key = 'wasprice';
+
+						var index = +itm.children[j].tagName.toLocaleLowerCase().replace(key, '')
+
+						if(!parse_data[key]) parse_data[key] = [];
+						parse_data[key][index-1] = itm.children[j].innerHTML
+					} else {
+						parse_data[key] = itm.children[j].innerHTML	
+					}
+				}
+
+				this.list_data.push(parse_data)
+			}
+		}
+		delete this.json_xml.data
+
+		this.json_xml.data = this.list_data;
+		// console.log(this.list_data, this.json_xml.data)
+
+	}
+	this.getPrice = function(srt){
+		var num = +srt;
+		var price = Math.floor(num);
+		var diff = num%price;
+
+		return diff > 0 ? num.toFixed(2) : num;
+	}
+
+	this.startBanner = function(data){
+		clearTimeout(this.timeoutClearText)
+		self.elem_banner.img_product.classList.remove('show');
+		self.elem_banner.img_product.style.backgroundImage = '';
+		parent.classList.add('show');
+
+		var saving = +data.saving;
+
+		var retail_diff = this.getPrice(data.retail); 
+
+		this.elem_banner.text_price.innerHTML = data.currencysymbol + retail_diff
+
+		this.elem_banner.brand.innerHTML = [data.brand,data.model].join(' - ')
+		this.elem_banner.center_price.innerHTML = data.currencysymbol + saving
+		this.elem_banner.desc.innerHTML = data.description
+		this.elem_banner.red_line_top.innerHTML = data.strapline1;
+
+		if(!data.wasprice ||(data.wasprice && !data.wasprice.length)){
+			this.removeWasprice(data)
+		} else {
+			this.addWasprice(data)
+		}
+		this.clearText();
+		var diff = this.timeoutClearText ? 0 : 1800;
+
+		this.timeoutClearText = setTimeout(function(){
+			self.animationText(0)
+		}, diff);
+
+		this.arr_text = data['feature'];
+
+		if(this.timeout_text){
+			clearTimeout(this.timeout_text);
+		}
+
+		var img = new Image();
+		img.src = data.productimageurl;
+		img.onload = function(){
+			self.elem_banner.img_product.style.backgroundImage = 'url('+this.src+')';
+			self.elem_banner.img_product.classList.add('show');
+		}
+	}
+
+	this.clearText = function(){
+		this.elem_banner.red_line_bottom[0].innerHTML = '';
+		this.elem_banner.red_line_bottom[1].innerHTML = '';
+
+
+		this.elem_banner.red_line_bottom[0].classList.remove('show');
+		this.elem_banner.red_line_bottom[1].classList.remove('show');
+
+
+		this.elem_banner.red_line_bottom[0].classList.remove('hide');
+		this.elem_banner.red_line_bottom[1].classList.remove('hide');
+	}
+
+	this.addWasprice = function(data){
+		this.banner.classList.remove('no_wasprice');
+		var list_was = this.elem_banner.list_was
+		while(list_was.children.length){
+			list_was.removeChild(list_was.children[0])
+		}
+
+		for(var i = 0; i < data.wasprice.length; i++){
+			var elem = createElem('div', 'itm_waspice', list_was);
+			var num = this.getPrice(data.wasprice[i]);
+			elem.innerHTML = data.currencysymbol + num
+		}
+	}
+	this.removeWasprice = function(){
+		this.banner.classList.add('no_wasprice');
+	}
+
+	this.animationText = function(index){
+		var arr_elem = this.elem_banner.red_line_bottom;
+		var arr_text = this.arr_text
+		var prev_itm = Math.abs((index-1)%2);
+		var index_itm = index%2;
+
+		var next_index = (index+1)%this.arr_text.length
+		// console.log(next_index, prev_itm)
+
+		if((prev_itm) >= 0){
+			arr_elem[prev_itm].classList.remove('show');
+			arr_elem[prev_itm].classList.add('hide');			
+		}
+
+		// if(index < this.arr_text.length){
+			arr_elem[index_itm].innerHTML = this.arr_text[index];
+			arr_elem[index_itm].classList.remove('hide');
+			arr_elem[index_itm].classList.add('show');
+		// }
+		
+
+
+		// if(index < this.arr_text.length){
+			var next_index = (index+1)%this.arr_text.length
+			this.timeout_text = setTimeout(self.animationText.bind(self, next_index),2000);
+		// } else {
+			// this.banner.classList.add('finish')
+			// this.timeout_text = false;
+		// }
+	}
+
+
+
+	function createElem(tag, class_name, par){
+		var elem = document.createElement(tag);
+		if(class_name){
+			elem.className = class_name
+		}
+		if(par){
+			par.appendChild(elem)
+		}
+
+		return elem
+	}
+
+	this.fullScreen = function() {
+		var element = document;//this.banner;
+		if(element.requestFullscreen) {
+			element.requestFullscreen();
+		} else if(element.webkitrequestFullscreen) {
+			element.webkitRequestFullscreen();
+		} else if(element.mozRequestFullscreen) {
+			element.mozRequestFullScreen();
+		}
+	}
+
+	this.init()
+}
